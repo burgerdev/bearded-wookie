d813d15b7091489ff7bc16148f042b5674272c5c
Markus Döring <webmaster@burgerdev.de>
2013-10-07 11:10:23 +0200

    finished excercises

--- exercises/10_flow_control.py
+++ exercises/10_flow_control.py
@@ -3,7 +3,86 @@
 # Author: Markus Döring
 
 
-# Flow Control
-# ============
+# Excercises
+# ==========
+#
+# Try to fill in your own code at the [CODE] markers.
+
+
+###############################################################################
+# a) Simple for
+#
+# Print all integers from 0 to 32 that are divisible by 2. The [CODE] markers 
+# denote the lines which should be replaced with meaningful instructions.
+# HINT: 
+#   - An integer n is divisible by to if n = 2*m holds for an integer m. 
+#   - You don't need to use `if-else` here if you use hint (1)
+
+print("a)")
+
+maxM = 1  #[CODE]
+for m in range(maxM):
+    n = 2  #[CODE]
+    print(n)
+
+print("")
+
+
+###############################################################################
+# b) Advanced for
+#
+# Print all strings in variable `myStrings` in upper case. 
+# HINT:
+#   - You can use any iterable (e.g., a list) as a replacement for `range()`
+
+myStrings = ["This", "is", "not", "the", "algorithm"]
+
+print("b)")
+
+#[CODE]
+
+print("")
+
+
+###############################################################################
+# c) Simple if-else
+# 
+# Print all integers from 0 to 32 that are divisible by 2.
+# HINT:
+#   - An integer n is divisible by 2 if and only if 
+#       n modulo 2 == 0
+#   - In python, you can use the modulo operator `%`
+#       n % 2 == 0 ?
+#
+
+print("c)")
+
+for n in range(32):
+    #[CODE]
+
+print("")
+
+###############################################################################
+# d) Advanced if-else
+# 
+# Print all integers from 0 to 32 that are divisible by 2, but not by 3.
+# HINT:
+#   - you can use the keywords `and`, `or`, `not` to work with booleans
+#       c = a and b
+#       c = b or not a
+#       ...
+#     Remember that `not` binds stronger than `and` or `or`!
+#
+
+
+print("d)")
+
+#[CODE]
+
+print("")
+
+
+
+
 
 
--- exercises/11_functions.py
+++ exercises/11_functions.py
@@ -36,9 +36,14 @@ print("")
 
 ###############################################################################
 # b) My first function
-
+#
 # Implement the `n!` function:
 #   fac(n) == n*(n-1)*(n-2)*...*2*1
+#
+# Hint: There are two main ways to implement the factorial function, you can try
+# both!
+#       - recursive: fac(1) = 1, fac(n+1) = (n+1)*fac(n)
+#       - iterative: a `for` loop
 
 def fac(n):
     f = 1
@@ -50,3 +55,28 @@ def fac(n):
 print("b)")
 print("fac(5) = {}".format(fac(5)))
 print("")
+
+###############################################################################
+# c) More functions
+#
+# Welcome back to the email address program. Create a function `isEmailAddress()`
+# that returns a boolean value for an input string. The value should only be
+# `True` if
+#       - the address contains exactly one '@' sign
+#       - the '@' sign is not the first character of the string
+#       - the address ends in one of the TLDs listed in variable `tlds`
+# Can you think of other requirements regarding email address validity?
+# Check your function on the example email addresses, and feel free to add more
+# examples that you would like to check.
+
+tlds = ['de', 'com', 'org', 'uk', 'at', 'ch', 'net']
+
+example_addresses = ['mail@example.org',  # valid
+                     'me@test.invalid',  # invalid
+                     'http://google.com',  # invalid
+                     'another@one.de',  # valid
+                     '@test.ch',  # invalid
+                     ]
+
+#[CODE]
+
