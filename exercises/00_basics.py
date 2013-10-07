2d0d41a4fa775670a5a41887105628ffd958e5b9
Markus Döring <webmaster@burgerdev.de>
2013-10-07 08:45:35 +0200

    updated exercises

--- exercises/00_basics.py
+++ exercises/00_basics.py
@@ -9,122 +9,60 @@
 #     (make sure that you are in the right directory)
 
 
-# Basics
-# ======
-
-# This is a commented line
-
-# `pass` is a so called 'zero operation', which does nothing
-pass  # You can also comment at the end of a line of code
-
-# This is a statement. It gets evaluated, but no assignment is taking
-# place here
-(5 + 4) * 2
-
-# This is an assignment to a variable. In Python, variables are declared by
-# assigning to them (you don't need to tell the interpreter that you are going
-# to use a variable in advance).
-n = 12
-
-# In principle, statements are constructed from
-#       - operations, like `+`, `*`, ...
-#       - primitives, like `0`, `1e9`, `42.0`, ...
-#       - variables that have been defined earlier
-#       - brackets for grouping `()`
-#       - function calls, like `print()`
-# Statements can be assigned to variables, or passed to functions
-example = (5 + 4) * 3
-example = example - (8 % 3)
-
-print("The content of the variable `example` is now")
-print(example)
-print("")
-
-print("The remainder of the division `example / 2` is")
-print(example % 2)
-print("")
-
-
-# Simple Types - Primitives
-# =========================
-#
-# Primitive types are basic building blocks of a computer program. There are a
-# few primitive types in python<1>, also called builtins:
-#       - None
-#       - boolean, one of `True` and `False`
-#       - integer, like `4`, `-31337`
-#       - float, like `41.0`
-#       - string, like `"Hello World"`
-#       - tuple, like `(15, 0, 3.4, -1, "abc")`
-#       - list, like `[15, 0, 3.4, -1, "abc"]`
-#       - dictionary, like `{'ham': 'egg', 'tree': 'bark', 'dog': 'bark', 0: 1}`
-#       - set, like `{23, 'ham', 12}`
-# The first six types are 'immutable', which means they cannot be modified by
-# the program.
-
-## Really basic types:
-myInteger = 1
-myFloat = 2.3
-
-## So called 'iterables', which can be accessed item-by-item:
-myString = "these are multiple characters"
-
-# Items of strings, tuples and lists can be accessed by their index. Indices
-# in python always start at 0!
-print("The first character of the string variable `myString` is:")
-print(myString[0])
-print("")
-
-# The statement `3:12` is 'slice notation', a range given as `start:stop`. Note
-# that the last item (the item at index 12) is not included.
-print("A sequence of characters inside variable `myString`:")
-print(myString[3:12])
-print("")
-
-# All iterables can be counted by using the function len()
-print("The lenght of variable `myString` is")
-print(len(myString))
-print("")
-
-
 # ==========
 # Excercises
 # ==========
 #
 # Try to fill in your own code at the [CODE] markers.
 
-# comment out the following two lines when you are ready for the excercises <2>
-import sys
-sys.exit(0)
-
-# HINT: commenting out would look like this:
-
-# import sys
-# sys.exit(0)
-
-# or like this:
-
-'''
-import sys
-sys.exit(0)
-'''
-
-
-# Simple Assignment
-# =================
+###############################################################################
+# a) Simple Assignment
+# ====================
 #
 # Try to assign the number `4` to the variable `a`, the
 # number `5` to the variable `b` and to assign the sum
 # of both `a` and `b` to the variable `c`. Print `c`.
 
+print("a)")
+print("Variable c is now:")
 
 #[CODE]
 
+print("")
 
+
+###############################################################################
+# b) Strings
+# ==========
+#
+# Let's say we have a program that validates email addresses. To do this, we 
+# have to check (among other things)
+#       - that it contains an '@' sign
+#       - that it has a German TLD ('.de' at the end)
+# Save the (boolean results) in the variables `containsAt` and `isGermanTLD`,
+#  and print them. 
+#
+# Hints:
+#       - You can check whether a string contains another string with the `in`
+#         keyword, and whether a string is equal to another one with `a == b` 
+#       - Strings (and other container types) can be indexed from the end by
+#         using negative numbers: `s[-3:-2]` would give the third-from-last
+#         character. To include the last character in your selection, you might
+#         need to use `len(s)`.
+
+print("b)")
+
+#[CODE]
+
+print("")
+
+
+###############################################################################
 # Tuples
 # ======
 #
 # We try the same indexing that we did for strings on tuples.
+
 
 myTuple = (4, 15, "Hello", 3.0, ("tuple in a tuple", 17), "foo")
 
@@ -134,11 +72,8 @@ print("The third element of tuple `myTuple` is:")
 print("The subtuple from index 3 to index 5 (including 5!) is:")
 #[CODE]
 
+print("The last two elements of the tuple are:")
+#[CODE]
 
 
-# <1> This is not to be taken too serious, rather as a
-# [Wittgenstein's ladder](http://en.wikipedia.org/wiki/Lie-to-children). The
-# concept of primitive data types is not really a part of python, instead 
-# everything in python is an object.
-# <2> These lines let the program exit, so that you just see the examples
-# when you run it for the first time.
+
--- exercises/02_strings.py
+++ exercises/02_strings.py
@@ -3,65 +3,8 @@
 # Author: Markus Döring
 
 
-# String Manipulation
-# ===================
-#
-# Now we will start playing around with strings. Remember
-# that the string class is immutable, which means that
-# you cannot change the characters of a given string. 
-# However, you can use string methods to create a new string
-# that is changed in the way you like. 
-#
-# You can find much information in the 
-# [documentation](http://docs.python.org/2/library/stdtypes.html#string-methods)
-
-
-# Strings (sequences of characters) are defined by putting
-# them into quotation marks. 
-# >>> a = "Hello"
-# >>> b = 'World'
-
-# The `print()` command does what it is supposed to do:
-# >>> print(a)
-# Hello
-
-# strings can be joined by the `+` operator
-# >>> greeting = a + " " + b + "!"
-# >>> print(greeting)
-# Hello World!
-
-# even multi-line strings are allowed with triple-quotation (multi-line strings
-# can also be used as code comments)
-# >>> longstring = """Hello,
-# World!"""
-# >>> print(longstring)
-# Hello,
-# World!
-
-# you can select substrings
-# >>> d = greeting[0:5]
-# >>> e = longstring[7:12]
-
-# strings are compared with the `==` (equals) operator
-# >>> print(a == d == "Hello")
-# True
-# >>> print(e == "World")
-# True
-
-# Strings are immutable, you can't assign to a string
-# >>> a = "Hello, world!"
-# >>> a[0:5] = "Adios"
-# TypeError: 'str' object does not support item assignment
-
-# Good string replacement example
-# >>> a = "Hello, world!"
-# >>> b = a.replace("Hello", "Goodbye")
-# >>> print(b)
-# Goodbye, world!
-
-# There are many more exciting string methods, just 
-# take a look around in the documentation. Recommended
-# reading:
+# There are many exciting string methods, just take a look around in the
+# documentation. Recommended reading:
 #	- str.strip()
 #	- str.split()
 #	- str.join()
